d5ceec2d83632469669fd3f2efcafbfc26826bd1
David <davidmarques.261299@gmail.com>
2026-03-11 19:18:52 -0300

    chore: Remoção da estrutura anterior

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-require __DIR__.'/api/v1/index.php';
+use Illuminate\Support\Facades\Route;
 
-/*
-use Illuminate\Http\Request;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-*/
+Route::prefix('v1')
+    ->name('api.')
+    ->group(function () {
+        // Route::apiResource('product', ProductController::class)
+        //     ->only(['index', 'show']);
+    });
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__.'/web/index.php';
+use Illuminate\Support\Facades\Route;
+
+Route::name('web.')
+    ->group(function () {});
