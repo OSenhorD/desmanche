756142b4705b55fe87bbf2ee8cbcd94c1dacd2e3
David <davidmarques.261299@gmail.com>
2026-03-11 01:55:45 -0300

    feat: Melhorias gerais

--- routes/web/index.php
+++ routes/web/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Livewire\User\Profile;
+use App\Livewire\Users\Index;
 use Illuminate\Support\Facades\Route;
 
 // TODO: Middleware para autenticado
@@ -15,3 +17,13 @@ Route::name('web.')
     ->group(function () {
         require __DIR__.'/admin.php';
     });
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+    Route::get('/users', Index::class)->name('users.index');
+
+    Route::get('/user/profile', Profile::class)->name('user.profile');
+});
+
+require __DIR__.'/auth.php';
