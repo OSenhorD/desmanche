21097fe438ee64beec1e030771f5b43c789243eb
David <davidmarques.261299@gmail.com>
2026-03-13 18:47:03 -0300

    feat: Login com ML

--- resources/views/components/pages/integrations/mercadolivre/conection.blade.php
+++ resources/views/components/pages/integrations/mercadolivre/conection.blade.php
@@ -18,15 +18,16 @@
             </x-filament::badge>
         </div>
 
-        <button
-            class="group relative w-full overflow-hidden rounded-xl border border-[#d4be00] bg-[#ffe600] px-4 py-3.25 text-[13.5px] font-[650] tracking-tight text-black shadow-[inset_0_1px_0_0_rgba(255,255,255,0.45),0_1px_2px_rgba(0,0,0,0.06)] transition-all hover:bg-[#ffea47] active:translate-y-px active:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.35)]">
+        <a href="https://staff-tribe-pichunter-humans.trycloudflare.com/api/v1/integrations/mercadolivre/login"
+            target="_blank"
+            class="group relative w-full block overflow-hidden rounded-xl border border-[#d4be00] bg-[#ffe600] px-4 py-3.25 text-[13.5px] font-[650] tracking-tight text-black shadow-[inset_0_1px_0_0_rgba(255,255,255,0.45),0_1px_2px_rgba(0,0,0,0.06)] transition-all hover:bg-[#ffea47] active:translate-y-px active:shadow-[inset_0_1px_0_0_rgba(255,255,255,0.35)]">
             <span class="relative z-10 flex items-center justify-center gap-2">
                 <x-fas-align-left class="h-4 w-4" />
                 Conectar com Mercado Livre
             </span>
             <span
                 class="pointer-events-none absolute inset-0 -translate-x-full bg-linear-to-r from-white/0 via-white/40 to-white/0 opacity-0 transition-all duration-700 group-hover:translate-x-full group-hover:opacity-100"></span>
-        </button>
+        </a>
 
         <p class="mt-3 px-3 pb-3 text-xs leading-4 text-zinc-500">
             Ao conectar, você autoriza a leitura e escrita em itens, pedidos, perguntas e estoque. Você pode
